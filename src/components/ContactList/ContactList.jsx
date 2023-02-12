137b6d47e4b4ce4c1f15d87ac694a50f363a7a6a
Natalya Karpova <knataly0513@gmail.com>
2023-02-13 00:07:47 +0200

    Update ContactList.jsx

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -2,13 +2,13 @@ import { Box } from "components/Box";
 import React from "react";
 import { ContactItem } from "./ContactItem";
 
-export const ContactList = ({ contacts, onDeleteContact }) => {
+export const ContactList = ({ contacts, onDeleteContact, children }) => {
     return (
         <Box
             as="ul"
             p={4}
             minHeight="contactListMinHeigth"
-        >
+        >{children}
             <ContactItem contacts={contacts} onDeleteContact={onDeleteContact}/>
         </Box>
     );
