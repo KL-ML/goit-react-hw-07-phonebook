64dac3290112b77e96e607213c8411ae0161acbe
Natalya Karpova <knataly0513@gmail.com>
2023-02-13 00:07:57 +0200

    Update contacts.thunk.js

--- src/redux/contactsFilter/contacts.thunk.js
+++ src/redux/contactsFilter/contacts.thunk.js
@@ -4,8 +4,8 @@ import axios from 'axios';
 axios.defaults.baseURL = "https://63e10cb6dd7041cafb3fc6e4.mockapi.io";
 
 
-export const getContactsThunk = createAsyncThunk(
-    'contacts',
+export const fetchContacts = createAsyncThunk(
+    'contacts/fetchAll',
     async (_, thunkAPI) => {
         try {
             const { data } = await axios.get('/contacts');
@@ -13,6 +13,30 @@ export const getContactsThunk = createAsyncThunk(
         } catch (e) {
             return thunkAPI.rejectWithValue(e.message);
         }
-        
+    }
+);
+
+export const addContact = createAsyncThunk(
+    'contacts/addContact',
+    async (currentContact, thunkAPI) => {
+        try {
+            const { id, name, number } = currentContact;
+            const { data } = await axios.post('/contacts', { id, name, number });
+            return data;
+        } catch (e) {
+            return thunkAPI.rejectWithValue(e.message);
+        }
+    }
+);
+
+export const deleteContact = createAsyncThunk(
+    'contacts/deleteContact',
+    async (contactId, thunkAPI) => {
+        try {
+            const { data } = await axios.delete(`/contacts/${contactId}`);
+            return data;
+        } catch (e) {
+            return thunkAPI.rejectWithValue(e.message);
+        }
     }
 );
