0dcd8477eaf8cac051d96e8a2816eaac37c48e7c
Natalya Karpova <knataly0513@gmail.com>
2023-02-13 00:07:42 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,4 @@
-import React, {
-  useEffect,
-  // useMemo,
-} from "react";
+import React, { useEffect, useMemo } from "react";
 import shortid from 'shortid';
 import { Box } from "./Box";
 import { ContactForm } from "./ContactForm";
@@ -9,18 +6,22 @@ import { ContactList } from "./ContactList";
 import { Filter } from "./Filter";
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import { useDispatch, useSelector } from "react-redux";
-import { deleteContactsAction, filterInputAction, setContactsAction } from "redux/contactsFilter/contacts.slice";
-import { getContactsThunk } from "redux/contactsFilter/contacts.thunk";
+import { filterInputAction } from "redux/contactsFilter/contacts.slice";
+import { addContact, fetchContacts, deleteContact } from "redux/contactsFilter/contacts.thunk";
+import {
+  getContacts,
+  getError,
+  getFilter,
+  getIsLoading
+} from "redux/contactsFilter/contacts.selectors";
 
 export const App = () => {
   const dispatch = useDispatch();
-
-  const filter = useSelector(state => state.contacts.filter);
-  const contacts = useSelector(state => state.contacts.contacts);
-  console.log('contacts: ', contacts);
-  // const loading = useSelector(state => state.contacts.contacts.loading);
-  // const error = useSelector(state => state.contacts.contacts.error);
-
+  const isLoading = useSelector(getIsLoading);
+  const error = useSelector(getError);
+  const filter = useSelector(getFilter);
+  const contacts = useSelector(getContacts);
+  
   const changeFilter = e => {
     dispatch(
       filterInputAction(
@@ -30,31 +31,27 @@ export const App = () => {
   };
  
   useEffect(() => {
-    dispatch(getContactsThunk());
+    dispatch(fetchContacts());
   }, [dispatch]);
     
   const formSubmitHandler = ({ name, number }) => {
-    const currentContact = { name: name, id: shortid.generate(), number: number }
+    const currentContact = { id: shortid.generate(), name: name, number: number }
     const contactDublicate = contacts.find(c => c.name === currentContact.name);
     if (contactDublicate) {
       Notify.failure(`${currentContact.name} is allready in contacts.`);
       return;
     }
-    dispatch(setContactsAction(
-      currentContact
-    ));
+    dispatch(addContact(currentContact));
   };
 
-  // const visibleContacts = useMemo(() => {
-  //   return contacts.filter(n =>
-  //     n.name.toLowerCase().includes(filter.toLowerCase()),
-  //   );
-  // }, [contacts, filter]);
+  const visibleContacts = useMemo(() => {
+    return contacts.filter(n =>
+      n.name.toLowerCase().includes(filter.toLowerCase()),
+    );
+  }, [contacts, filter]);
   
-  const deleteContact = id => {
-    dispatch(deleteContactsAction(
-      id
-    ));
+  const deleteCurrentContact = id => {
+    dispatch(deleteContact(id));
   };
 
   return (
@@ -80,8 +77,12 @@ export const App = () => {
         value={filter}
         onChange={changeFilter} />
       <ContactList
-        contacts={contacts}
-        onDeleteContact={deleteContact} />
+        contacts={visibleContacts}
+        onDeleteContact={deleteCurrentContact}>
+        {isLoading && !error && <p>Loading...</p>}
+        {!isLoading && !error && <p>Contacts list:</p>}
+        {!isLoading && error && <p>Sorry, something goe's wrong</p>}
+        </ContactList>
     </Box>
   );
 };
