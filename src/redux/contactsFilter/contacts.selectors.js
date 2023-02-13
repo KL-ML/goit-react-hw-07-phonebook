7b5c6170851637daa72e025854410e1db435c219
Natalya Karpova <knataly0513@gmail.com>
2023-02-13 10:42:11 +0200

    Update contacts.selectors.js

--- src/redux/contactsFilter/contacts.selectors.js
+++ src/redux/contactsFilter/contacts.selectors.js
@@ -1,7 +1,7 @@
-export const getContacts = state => state.contacts.contacts.items;
+export const selectContacts = state => state.contacts.contacts.items;
 
-export const getIsLoading = state => state.contacts.contacts.isLoading;
+export const selectIsLoading = state => state.contacts.contacts.isLoading;
 
-export const getError = state => state.contacts.contacts.error;
+export const selectError = state => state.contacts.contacts.error;
 
-export const getFilter = state => state.contacts.filter;
+export const selectFilter = state => state.contacts.filter;
